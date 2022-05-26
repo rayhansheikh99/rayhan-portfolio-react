a705bef6e662b12678069d0ecd13f5dfffabb696
rayhansheikh99 <rsheikh201031@msce.uiu.ac.bd>
2022-05-26 23:11:46 +0600

    portfolio and testimonials section added

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,9 +1,70 @@
 import React from 'react'
 import "./portfolio.css"
+import img1 from "../../assets/img1.png"
 
 const Portfolio = () => {
   return (
-    <section id='portfolio'>Portfolio</section>
+    <section id='portfolio'>
+      <h5>My Recent Works</h5>
+      <h2>Portfolio</h2>
+
+      <div className="container portfolio-container">
+        <article className="portfolio-item">
+          <div className="portfolio-item-img port-img">
+          </div>
+          <h3>Portfolio Title</h3>
+          <div className="portfolio-item-cta">
+          <a href="#" className="btn" target="_blank">GitHub</a>
+          <a href="#" target="_blank" className="btn btn-primary">Live Demo</a>
+          </div>
+        </article>
+        <article className="portfolio-item">
+          <div className="portfolio-item-img port-img">
+          </div>
+          <h3>Portfolio Title</h3>
+          <div className="portfolio-item-cta">
+          <a href="#" className="btn" target="_blank">GitHub</a>
+          <a href="#" target="_blank" className="btn btn-primary">Live Demo</a>
+          </div>
+        </article>
+        <article className="portfolio-item">
+          <div className="portfolio-item-img port-img">
+          </div>
+          <h3>Portfolio Title</h3>
+          <div className="portfolio-item-cta">
+          <a href="#" className="btn" target="_blank">GitHub</a>
+          <a href="#" target="_blank" className="btn btn-primary">Live Demo</a>
+          </div>
+        </article>
+        <article className="portfolio-item">
+          <div className="portfolio-item-img port-img">
+          </div>
+          <h3>Portfolio Title</h3>
+          <div className="portfolio-item-cta">
+          <a href="#" className="btn" target="_blank">GitHub</a>
+          <a href="#" target="_blank" className="btn btn-primary">Live Demo</a>
+          </div>
+        </article>
+        <article className="portfolio-item">
+          <div className="portfolio-item-img port-img">
+          </div>
+          <h3>Portfolio Title</h3>
+          <div className="portfolio-item-cta">
+          <a href="#" className="btn" target="_blank">GitHub</a>
+          <a href="#" target="_blank" className="btn btn-primary">Live Demo</a>
+          </div>
+        </article>
+        <article className="portfolio-item">
+          <div className="portfolio-item-img port-img">
+          </div>
+          <h3>Portfolio Title</h3>
+          <div className="portfolio-item-cta">
+          <a href="#" className="btn" target="_blank">GitHub</a>
+          <a href="#" target="_blank" className="btn btn-primary">Live Demo</a>
+          </div>
+        </article>
+      </div>
+    </section>
   )
 }
 
--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -1,9 +1,55 @@
 import React from 'react'
 import "./testimonials.css"
+import avatar1 from '../../assets/avatar1.png'
+
+const data = [
+  {
+    avatar: avatar1,
+    name: 'John Lewis',
+    review: 'Dolor sit amet consectetur adipisicing elit. Fugiat soluta incidunt doloremque. Eaque ipsa saepe inventore corrupti sed! Tenetur, reprehenderit?'
+  },
+  {
+    avatar: avatar1,
+    name: 'David Warner',
+    review: 'Dolor sit amet consectetur adipisicing elit. Fugiat soluta incidunt doloremque. Eaque ipsa saepe inventore corrupti sed! Tenetur, reprehenderit?'
+  },
+  {
+    avatar: avatar1,
+    name: 'John Lewis',
+    review: 'Dolor sit amet consectetur adipisicing elit. Fugiat soluta incidunt doloremque. Eaque ipsa saepe inventore corrupti sed! Tenetur, reprehenderit?'
+  },
+  {
+    avatar: avatar1,
+    name: 'John Lewis',
+    review: 'Dolor sit amet consectetur adipisicing elit. Fugiat soluta incidunt doloremque. Eaque ipsa saepe inventore corrupti sed! Tenetur, reprehenderit?'
+  },
+]
 
 const Testimonials = () => {
   return (
-    <section id='testimonials'>Testimonials</section>
+    <section id='testimonials'>
+      <h5>Review from Clients</h5>
+      <h2>Testimonials</h2>
+
+      <div className="container testimonials-container">
+        {
+          data.map(({avatar,name,review},index) => {
+            return (
+              <article key={index} className="testimonial">
+                <div className="client-avatar">
+                  <img src={avatar} alt="avatar-one" />
+                </div>
+                  <h5 className="client-name">{name} </h5>
+                  <small className="client-review">
+                    {review}
+                  </small>
+              </article>
+            )
+          })
+        }
+        
+      </div>
+    </section>
   )
 }
 
