fb7b507e47872e2b97c44b79e250d8a686d7c0d9
rayhansheikh99 <rsheikh201031@msce.uiu.ac.bd>
2022-05-29 00:37:07 +0600

    favicon added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,13 +3,14 @@ import Cta from './Cta'
 import "./header.css"
 import ME from "../../assets/rayhan1.png"
 import HeaderSocials from './HeaderSocials'
+import 'animate.css';
 
 const Header = () => {
   return (
     <header>
       <div className="container header-container">
-        <h5>Hello I'm</h5>
-        <h1>Rayhan Sheikh</h1>
+        <h5 className="animate__animated animate__fadeInLeft">Hello I'm</h5>
+        <h1 className="animate__animated animate__bounce animate__delay-2s animate__repeat-2">Rayhan Sheikh</h1>
         <h5 className="text-light">Junior Fullstack Developer</h5>
         <Cta/>
         <HeaderSocials/>
